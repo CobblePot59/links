5068ea3f570e43cda22a75b284fecd30df63fbb1
CobblePot59 <38343920+CobblePot59@users.noreply.github.com>
2022-05-06 18:07:13 +0200

    Update app.py

--- volumes/app/app.py
+++ volumes/app/app.py
@@ -32,12 +32,12 @@ def login():
         if request.method == 'GET':
             return render_template('login.html')
         else:
-            login = str(request.form['login'])
-            password = str(request.form['password'])
+            login = request.form['login']+'@'+app.config['LDAP_DOMAIN']
+            password = request.form['password']
             q = ldap.bind_user(login, password)
             if password and q == True:
                 session['status'] = True
-                session['login'] = login
+                session['login'] = login.split('@')[0]
                 return redirect(url_for('index'))
             else:
                 return 'Bad Login '
